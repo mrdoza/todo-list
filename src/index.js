fab5663c6b2eb5052e4d1692f0592688516e8731
mrdoza <marc.mendoza@gmail.com>
2024-06-30 13:23:32 -0500

    Initial commint setup all the files and folders tested the dependencies setup the webpack initial css and html layout

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./style.css"
+
+const content = "Hello, Webpack!";
+const menu = document.querySelector('.menu');
+menu.innerHTML = content;
+
+const listItems = "my List";
+const list = document.querySelector('.content');
+list.innerHTML = listItems;
