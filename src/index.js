e07188efaa70e59e75460b0351c2e1729a774ff9
mrdoza <marc.mendoza@gmail.com>
2024-07-02 20:48:24 -0500

    Form commit added a form to the index.html file added minimal styling to the form

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import { compareAsc, format } from "date-fns";
 
 const content = "Hello, Webpack!";
 const menu = document.querySelector(".menu");
-menu.innerHTML = content;
+// menu.innerHTML = content;
 
 const listItems = "my List";
 const list = document.querySelector(".content");
-list.innerHTML = listItems;
+//list.innerHTML = listItems;
 
-format(new Date(2014, 1, 11), "yyyy-MM-dd");
+const gate = format(new Date(2014, 4, 21), "MMM-dd-yyyy");
 //=> '2014-02-11'
 
-console.log(Date);
+console.log(gate);
 
 const dates = [
   new Date(1995, 6, 2),
   new Date(1987, 1, 11),
   new Date(1989, 6, 10),
 ];
-dates.sort(compareAsc);
+console.log(dates.sort(compareAsc));
