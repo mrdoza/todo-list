c29a970491377e974e98dc43e5e883162e6d79c2
mrdoza <marc.mendoza@gmail.com>
2024-07-02 19:30:08 -0500

    add a new dependency added date-fns

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import "./style.css"
+import "./style.css";
+import { compareAsc, format } from "date-fns";
 
 const content = "Hello, Webpack!";
-const menu = document.querySelector('.menu');
+const menu = document.querySelector(".menu");
 menu.innerHTML = content;
 
 const listItems = "my List";
-const list = document.querySelector('.content');
+const list = document.querySelector(".content");
 list.innerHTML = listItems;
+
+format(new Date(2014, 1, 11), "yyyy-MM-dd");
+//=> '2014-02-11'
+
+const dates = [
+  new Date(1995, 6, 2),
+  new Date(1987, 1, 11),
+  new Date(1989, 6, 10),
+];
+dates.sort(compareAsc);
