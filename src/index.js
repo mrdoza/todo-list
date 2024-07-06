fc972df86d772bd2a89911ab34e3dc1033ac9e2a
mrdoza <marc.mendoza@gmail.com>
2024-07-06 11:10:06 -0500

    js update
    added event listener to the form
    The events are being added to the rightPane
    The form is being reset after the submit event

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
 import "./style.css";
 import { compareAsc, format } from "date-fns";
 
-const content = "Hello, Webpack!";
-const menu = document.querySelector(".menu");
-// menu.innerHTML = content;
+document.addEventListener("DOMContentLoaded", (event) => {
+  const form = document.getElementById("myForm");
+  const rightPane = document.getElementById("rightPane");
+  const dataArray = [];
 
-const listItems = "my List";
-const list = document.querySelector(".content");
-//list.innerHTML = listItems;
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const formObject = {};
+    formData.forEach((value, key) => {
+      formObject[key] = value;
+    });
+    const priority = document.querySelector(
+      'input[name="priority"]:checked'
+    ).value;
+    formObject["priority"] = priority;
 
-const gate = format(new Date(2014, 4, 21), "MMM-dd-yyyy");
-//=> '2014-02-11'
+    dataArray.push(formObject);
+    console.log(dataArray);
 
-console.log(gate);
-
-const dates = [
-  new Date(1995, 6, 2),
-  new Date(1987, 1, 11),
-  new Date(1989, 6, 10),
-];
-console.log(dates.sort(compareAsc));
+    const entryDiv = document.createElement("div");
+    entryDiv.className = "entry";
+    entryDiv.innerHTML = `
+        <span style="display: inline;"><strong>Name:</strong> ${formObject.task_name}</span>
+        <span style="display: inline;"><strong>Desc:</strong> ${formObject.task_desc}</span> <br />
+        <span><strong>Due:</strong> ${formObject.due_date}</span>
+        <span><strong>Priority:</strong> ${formObject.priority}</span>
+      `;
+    rightPane.appendChild(entryDiv);
+    form.reset();
+  });
+});
